527dc2de7de0713635e3e4fc3631dcab708c6666
rip@devco.net <rip@devco.net@2f60ba10-c8c0-11de-a675-93be1f20d5c2>
2009-12-01 18:57:56 +0000

    create 0.2 branch

---
+++ branches/0.2/lib/mcollective.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'stomp'
+require 'logger'
+require 'timeout'
+require 'digest/md5'
+require 'optparse'
+require 'singleton'
+require 'socket'
+
+# == The Marionette Collective
+#
+# Framework to build and run Systems Administration agents running on a 
+# publish/subscribe middleware system.  The system allows you to treat your
+# network as the only true source of the state of your platform via discovery agents
+# and allow you to run agents matching discovery criteria.
+#
+# For an overview of the idea behind this and what it enables please see:
+#   http://www.devco.net/archives/2009/10/18/middleware_for_systems_administration.php
+module MCollective
+    autoload :Config, "mcollective/config"    
+    autoload :Log, "mcollective/log"    
+    autoload :Runner, "mcollective/runner"    
+    autoload :Agents, "mcollective/agents"    
+    autoload :Client, "mcollective/client"    
+    autoload :Util, "mcollective/util"    
+    autoload :Optionparser, "mcollective/optionparser"
+    autoload :Connector, "mcollective/connector"
+    autoload :Security, "mcollective/security"
+    autoload :Facts, "mcollective/facts"
+    autoload :Registration, "mcollective/registration"
+    autoload :PluginManager, "mcollective/pluginmanager"
+end
+
+# vi:tabstop=4:expandtab:ai
