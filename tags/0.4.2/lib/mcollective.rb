4c05a066ff486c4d1ce569a66d6571d90f310a41
rip@devco.net <rip@devco.net@2f60ba10-c8c0-11de-a675-93be1f20d5c2>
2010-01-13 18:41:47 +0000

    Update issue 33 Tag release 0.4.2

---
+++ tags/0.4.2/lib/mcollective.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'stomp'
+require 'logger'
+require 'timeout'
+require 'digest/md5'
+require 'optparse'
+require 'singleton'
+require 'socket'
+
+# == The Marionette Collective
+#
+# Framework to build and run Systems Administration agents running on a 
+# publish/subscribe middleware system.  The system allows you to treat your
+# network as the only true source of the state of your platform via discovery agents
+# and allow you to run agents matching discovery criteria.
+#
+# For an overview of the idea behind this and what it enables please see:
+#   http://www.devco.net/archives/2009/10/18/middleware_for_systems_administration.php
+module MCollective
+    # Exceptions for the RPC system
+    class RPCError<StandardError;end
+    class UnknownRPCAction<RPCError;end
+    class MissingRPCData<RPCError;end
+    class InvalidRPCData<RPCError;end
+    class UnknownRPCError<RPCError;end
+
+    autoload :Config, "mcollective/config"    
+    autoload :Log, "mcollective/log"    
+    autoload :Runner, "mcollective/runner"    
+    autoload :Agents, "mcollective/agents"    
+    autoload :Client, "mcollective/client"    
+    autoload :Util, "mcollective/util"    
+    autoload :Optionparser, "mcollective/optionparser"
+    autoload :Connector, "mcollective/connector"
+    autoload :Security, "mcollective/security"
+    autoload :Facts, "mcollective/facts"
+    autoload :Registration, "mcollective/registration"
+    autoload :PluginManager, "mcollective/pluginmanager"
+    autoload :RPC, "mcollective/rpc"
+end
+
+# vi:tabstop=4:expandtab:ai
---
+++ tags/0.4.2/lib/mcollective/security/base.rb
@@ -0,0 +1,140 @@
+module MCollective
+    module Security
+        # This is a base class the other security modules should inherit from
+        # it handles statistics and validation of messages that should in most 
+        # cases apply to all security models.
+        # 
+        # To create your own security plugin you should provide a plugin that inherits
+        # from this and provides the following methods:
+        #
+        # decodemsg      - Decodes a message that was received from the middleware
+        # encodereply    - Encodes a reply message to a previous request message
+        # encoderequest  - Encodes a new request message
+        # validrequest?  - Validates a request received from the middleware
+        # 
+        # Specifics of each of these are a bit fluid and the interfaces for this is not
+        # set in stone yet, specifically the encode methods will be provided with a helper
+        # that takes care of encoding the core requirements.  The best place to see how security
+        # works is by looking at the provided MCollective::Security::PSK plugin.
+        class Base
+            attr_reader :stats
+    
+            # Register plugins that inherits base
+            def self.inherited(klass)
+                PluginManager << {:type => "security_plugin", :class => klass.to_s}
+            end
+
+            # Initializes configuration and logging as well as prepare a zero'd hash of stats
+            # various security methods and filter validators should increment stats, see MCollective::Security::Psk for a sample
+            def initialize
+                @config = Config.instance
+                @log = Log.instance
+    
+                @stats = {:passed => 0,
+                          :filtered => 0,
+                          :validated => 0,
+                          :unvalidated => 0}
+            end
+    
+            # Takes a Hash with a filter in it and validates it against host information.
+            #
+            # At present this supports filter matches against the following criteria:
+            #
+            # - puppet_class|cf_class - Presence of a configuration management class in
+            #                           the file configured with classesfile
+            # - agent - Presence of a MCollective agent with a supplied name
+            # - fact - The value of a fact avout this system
+            # - identity - the configured identity of the system
+            #
+            # TODO: Support REGEX and/or multiple filter keys to be AND'd
+            def validate_filter?(filter)
+                failed = 0
+                passed = 0
+    
+                passed = 1 if Util.empty_filter?(filter)
+    
+                filter.keys.each do |key|
+                    case key
+                        when /puppet_class|cf_class/
+                            filter[key].each do |f|
+                                @log.debug("Checking for class #{f}")
+                                if Util.has_cf_class?(f) then
+                                    @log.debug("Passing based on configuration management class #{f}")
+                                    passed += 1
+                                else
+                                    @log.debug("Failing based on configuration management class #{f}")
+                                    failed += 1
+                                end
+                            end
+            
+                        when "agent"
+                            filter[key].each do |f|
+                                if Util.has_agent?(f) || f == "mcollective"
+                                    @log.debug("Passing based on agent #{f}")
+                                    passed += 1
+                                else
+                                    @log.debug("Failing based on agent #{f}")
+                                    failed += 1
+                                end
+                            end
+            
+                        when "fact"
+                            filter[key].each do |f|
+                                if Util.has_fact?(f[:fact], f[:value]) 
+                                    @log.debug("Passing based on fact #{f[:fact]} = #{f[:value]}")
+                                    passed += 1
+                                else
+                                    @log.debug("Failing based on fact #{f[:fact]} = #{f[:value]}")
+                                    failed += 1
+                                end
+                            end
+    
+                        when "identity"
+                            filter[key].each do |f|
+                                if Util.has_identity?(f)
+                                    @log.debug("Passing based on identity = #{f}")
+                                    passed += 1
+                                else
+                                    @log.debug("Failed based on identity = #{f}")
+                                    failed += 1
+                                end
+                            end
+                    end
+                end
+            
+                if failed == 0 && passed > 0
+                    @log.debug("Message passed the filter checks")
+    
+                    @stats[:passed] += 1
+                    return true
+                else
+                    @log.debug("Message failed the filter checks")
+    
+                    @stats[:filtered] += 1
+                    return false
+                end
+            end
+        end
+
+        # Security providers should provide this, see MCollective::Security::Psk
+        def validrequest?(req)
+            @log.error("validrequest? is not implimented in #{this.class}")
+        end
+
+        # Security providers should provide this, see MCollective::Security::Psk
+        def encoderequest(sender, target, msg, filter={})
+            @log.error("encoderequest is not implimented in #{this.class}")
+        end
+
+        # Security providers should provide this, see MCollective::Security::Psk
+        def encodereply(sender, target, msg, filter={})
+            @log.error("encodereply is not implimented in #{this.class}")
+        end
+
+        # Security providers should provide this, see MCollective::Security::Psk
+        def decodemsg(msg)
+            @log.error("decodemsg is not implimented in #{this.class}")
+        end
+    end
+end
+# vi:tabstop=4:expandtab:ai
